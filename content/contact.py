f9f52887a32be0910a4481de4b9dd475da719bf0
Randy Duodu <52050586+iSOLveIT@users.noreply.github.com>
2020-04-30 21:29:21 +0000

    Update contact.py

--- content/contact.py
+++ content/contact.py
@@ -5,9 +5,9 @@ from datetime import datetime as dt
 
 def sendEmail(_name, _subject, _email, _message):
     # SEND EMAIL
-    _recipient = 'agmschool.management@gmail.com'
-    msg = Message(_subject, sender=('AGMS Contact', 'isolveitgroup@gmail.com'), recipients=[_recipient])
-    assert msg.sender == "AGMS Contact <isolveitgroup@gmail.com>"
+    _recipient = 'receiver@email.com'
+    msg = Message(_subject, sender=('AGMS Contact', 'sender@email.com'), recipients=[_recipient])
+    assert msg.sender == "AGMS Contact <sender@email.com>"
     msg.body = f'''{_message}
     
 Sender's Name: {_name}
@@ -21,8 +21,8 @@ Date Sent:  {dt.now().strftime('%B %d, %Y, %H:%M ') + 'GMT'}
 def replyMessage(_email, _sender):
     # REPLY EMAIL
     _subj = 'Message Received'
-    msg = Message(_subj, sender=('AGMS Contact', 'isolveitgroup@gmail.com'), recipients=[_email])
-    assert msg.sender == "AGMS Contact <isolveitgroup@gmail.com>"
+    msg = Message(_subj, sender=('AGMS Contact', 'sender@email.com'), recipients=[_email])
+    assert msg.sender == "AGMS Contact <sender@email.com>"
     msg.body = f'''Hello {_sender},
 The message sent by you to AGMS Contact has been received. AGMS Contact will contact you within 24 hours.
 
